1e9d60fc2d4076419513b25dd86a6abf8bbba1ab
Kevin Hung <kevincjhung@gmail.com>
2022-12-25 08:44:47 -0800

    init

---
+++ database.js
@@ -0,0 +1,77 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+
+// id: 1, 
+// key: 1,
+// date: "Mon Sep 19 2022 09:23:01 GMT-0700 (Pacific Daylight Time)",
+// text: "Helped grandma cross the street",
+// rating: 6
+
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST, 
+    user: process.env.MYSQL_USER, 
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT,
+}).promise()
+
+
+// returns all the moods in the mood table as array of objects
+export async function getMoods(){
+    const [result] = await pool.query("SELECT * FROM moods");
+    return result;
+}
+
+
+export async function deleteMood(id){
+    let query = `
+        DELETE 
+        FROM moods
+        WHERE id = ?`;
+    
+    const [result] = await pool.query(query, [id]);
+    return result;
+}
+
+
+export async function updateMood(notes, rating, id){
+    let query = `
+        UPDATE moods
+        SET
+        notes = ?,
+        rating = ?
+        WHERE id = ?`;
+
+    return await pool.query(query, [notes, rating, id]);
+}
+
+
+export async function insertMood(notes, rating){
+    let query = `
+        INSERT INTO moods (notes, rating)
+        VALUES (?, ?)`;
+
+    return await pool.query(query, [notes, rating]);
+}
+
+
+export async function seedDatabase(){
+   let query = `
+   INSERT INTO 
+     moods(notes, rating, created) 
+   VALUES 
+    ('Contemplate existence',6,'2022-04-12 02:23:22'), 
+    ('Go whale watching in Fiji',7,'2022-04-12 02:23:22'), 
+    ('Go camping in Hawaii',9,'2022-04-12 02:23:22'), 
+    ('Go backpacking in Colombia',9,'2022-04-12 02:23:22'), 
+    ('Hike a mountain',8,'2022-04-12 02:23:22'), 
+    ('Go to the office',7,'2022-02-26 02:23:22'), 
+    ('Move to the beach to become a professional surfer',4,'2022-04-12 02:23:22'), 
+    ('Apply for medical school',8,'2022-08-12 02:23:22'), 
+    ('Go to see Swedish House Mafia',9,'2022-04-17 02:23:22')`
+
+   await pool.query(query)
+}
